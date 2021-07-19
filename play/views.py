70dcc55d2bfaad6cc67ff5b977b2753e4b07bd7e
SureshChandrappa <suresh.k.chandrappa@gmail.com>
2021-07-19 15:31:39 +0530

    second chapter

--- play/views.py
+++ play/views.py
@@ -3,6 +3,11 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+def calculate():
+    x=2
+    return x
+
 def sya_hello(request):
     #return HttpResponse("Hello world")
+    x=calculate()
     return render(request,'hello.html',{'name':'Suresh'})
