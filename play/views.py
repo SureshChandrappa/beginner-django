017b8e891e8aa5966487c3a1a404bf59cf03c61b
SureshChandrappa <suresh.k.chandrappa@gmail.com>
2021-07-21 14:55:58 +0530

    retreving object

--- play/views.py
+++ play/views.py
@@ -1,13 +1,23 @@
+from django.core import exceptions
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.exceptions import ObjectDoesNotExist
+from store.models import Product
 
 # Create your views here.
 
-def calculate():
-    x=2
-    return x
 
 def sya_hello(request):
     #return HttpResponse("Hello world")
-    x=calculate()
+    #queryset = Product.objects.all()
+    try:
+        product = Product.objects.get(pk=0)
+    except ObjectDoesNotExist:
+        pass
+    #product = Product.objects.filter(pk=1).first()
+    exist = Product.objects.filter(pk=1).exists()
+    #list(query_set)
+    #for product in query_set:
+    #    print(product)
+
     return render(request,'hello.html',{'name':'Suresh'})
