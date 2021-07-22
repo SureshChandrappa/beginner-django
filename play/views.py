892b37883ce4b67e9d643cf2342f234b5afb7a0f
SureshChandrappa <suresh.k.chandrappa@gmail.com>
2021-07-22 15:00:15 +0530

    adddinfg F objects

--- play/views.py
+++ play/views.py
@@ -1,5 +1,5 @@
 from django.core import exceptions
-from django.db.models import Q
+from django.db.models import Q, F
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
@@ -13,6 +13,8 @@ def sya_hello(request):
     #queryset = Product.objects.filter(unit_price__gt=10,unit_price__lt=20)
     #queryset = Product.objects.filter(unit_price__gt=10).filter(unit_price__lt=20)
     # inventory >10 OR unitprice < 20 for this we use Q
-    queryset = Product.objects.filter(Q(inventory__lt=10) | Q(unit_price__lt=20))
+    #queryset = Product.objects.filter(Q(inventory__lt=10) | Q(unit_price__lt=20))
+    #using F object to compare
+    queryset = Product.objects.filter(inventory=F('collection__id'))
 
     return render(request,'hello.html',{'name':'Suresh','products':list(queryset)})
