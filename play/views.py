8720cc882e02d5a7543be5080f395543366167d6
SureshChandrappa <suresh.k.chandrappa@gmail.com>
2021-07-22 19:05:11 +0530

    selecting field

--- play/views.py
+++ play/views.py
@@ -3,14 +3,16 @@ from django.db.models import Q, F
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
-from store.models import Product
+from store.models import Product, OrderItems
 
 # Create your views here.
 
 
 def sya_hello(request):
     #Sort
-    #queryset = Product.objects.order_by('title')
-    queryset = Product.objects.order_by('inventory','-title').reverse()
+    # .values to select using title
+    queryset =Product.objects.filter(id__in= OrderItems.objects.values('product_id').distinct()).order_by('title')
+
+
 
     return render(request,'hello.html',{'name':'Suresh','products':list(queryset)})
