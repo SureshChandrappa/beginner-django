e35c74b34e1e46ea34e788774bc2808604886939
SureshChandrappa <suresh.k.chandrappa@gmail.com>
2021-07-22 15:05:35 +0530

    sort

--- play/views.py
+++ play/views.py
@@ -9,12 +9,8 @@ from store.models import Product
 
 
 def sya_hello(request):
-    # inventory > 10 AND unit_price <20
-    #queryset = Product.objects.filter(unit_price__gt=10,unit_price__lt=20)
-    #queryset = Product.objects.filter(unit_price__gt=10).filter(unit_price__lt=20)
-    # inventory >10 OR unitprice < 20 for this we use Q
-    #queryset = Product.objects.filter(Q(inventory__lt=10) | Q(unit_price__lt=20))
-    #using F object to compare
-    queryset = Product.objects.filter(inventory=F('collection__id'))
+    #Sort
+    #queryset = Product.objects.order_by('title')
+    queryset = Product.objects.order_by('inventory','-title').reverse()
 
     return render(request,'hello.html',{'name':'Suresh','products':list(queryset)})
